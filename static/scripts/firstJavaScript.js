3536ab7cfbbaab060dc983ff889682478a26cc4c
LeviDomingos <92650678+LeviDomingos@users.noreply.github.com>
2022-05-01 19:07:33 +0100

    working on responsive design
    making the page responsive on mobile device

--- static/scripts/firstJavaScript.js
+++ static/scripts/firstJavaScript.js
@@ -1,30 +1,27 @@
 /* paste this line in verbatim */
-var cells = 12;
+var cells = 16;
 var cell;
 
+window.onload = function(){
 
- window.onload = function(){
+  let startplaying = document.getElementById("startplaying")  
+  let idgame = document.getElementById("idgame");
+  let createboard= document.getElementById("createboard");
 
-let startplaying = document.getElementById("startplaying")  
-let idgame = document.getElementById("idgame");
-let createboard= document.getElementById("createboard");
+  startplaying.addEventListener("click", function(){
+    $("#createboard").removeClass("btn-off-visible");
+    $("#createboard").addClass("btn-on-visible");
+    for(var i = 1; i <= cells; i ++){
+      cell = $('<div>').addClass('box').attr('data-cell', i).text("");
+      cell.addClass('box');
+      $('#idgame').append(cell);
+    }
+  });
 
-startplaying.addEventListener("click", function(){
-  $("#createboard").removeClass("btn-off-visible");
-  $("#createboard").addClass("btn-on-visible");
-});
-
-createboard.addEventListener("click", function(){
-
-for(var i = 1; i <= cells; i ++){
-cell = $('<div>').addClass('box').attr('data-cell', i).text("");
-cell.addClass('box');
-$('#idgame').append(cell);
-}
-
-});
+  createboard.addEventListener("click", function(){
 
 
+  });
 };
 
 
@@ -53,50 +50,6 @@ $('#idgame').append(cell);
 
 
 
-
-
-
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
  
  
  
