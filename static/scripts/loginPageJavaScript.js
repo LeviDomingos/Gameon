9842d0cd6006e41e2d93e2d2cf7fc5b89a743d37
LeviDomingos <92650678+LeviDomingos@users.noreply.github.com>
2022-06-12 22:46:19 +0100

    javascript login page
    validating login fields

--- static/scripts/loginPageJavaScript.js
+++ static/scripts/loginPageJavaScript.js
@@ -1,19 +1,38 @@
+let nickname = "";
+let email = "";
+let password = "";
+let firstNameOrEmail = "";
+let personalPassword = "";
+
 window.onload = function() {
 
-  const loginMemberButton = document.getElementById("submitbt");
+  let loginMemberButton = document.getElementById("submitbt");
 
   /**for the login page  */
   loginMemberButton.addEventListener("click", function() {
-    let nickname = window.localStorage.nickname;
-    let email = window.localStorage.email;
-    let password = window.localStorage.password;
+    
+    nickname = localStorage.nickname;
+    email = localStorage.email;
+    password = localStorage.password;
         
-    let firstNameOrEmail = document.getElementById("nickname");
-    let personalPassword = document.getElementById("pwd");
+    firstNameOrEmail = document.getElementById("nickname");
+    personalPassword = document.getElementById("pwd");
       
-    if(firstNameOrEmail.innerText === nickname || firstNameOrEmail.innerText === email && personalPassword.innerText === password) {
-      alert("Welcome " + nickname || email);
+    if(validateFields() === false) {
+      alert("All the Fields must be filled")
+    }
+    else {
+     if(firstNameOrEmail.innerText === nickname || firstNameOrEmail.innerText === email && personalPassword.innerText === password) {
+       alert("Welcome " + nickname || email);
+      }
     }
   });
+
+  function validateFields(nickname, password) {
+    if(nickname === "" || password === ""){
+      return false;
+    }
+
+  }
 
 }
